d7229323a55c4a4742c8022ad82bc6c99d9e3f18
Yoav Farhi <yoav@farhi.org>
2018-12-23 20:45:14 +0200

    Update tests. Currently failing. Will add more

--- client/lib/phone-validation/test/index.jsx
+++ client/lib/phone-validation/test/index.jsx
@@ -12,39 +12,48 @@ import phoneValidation from '..';
 
 describe( 'Phone Validation Library', () => {
 	test( 'should fail an empty number', () => {
-		assert.equal( 'phone_number_empty', phoneValidation( '' ).error );
+		assert.strictEqual( phoneValidation( '' ).error, 'phone_number_empty' );
 	} );
 	test( 'should fail a short number', () => {
-		assert.equal( 'phone_number_too_short', phoneValidation( '+1234567' ).error );
+		assert.strictEqual( phoneValidation( '+1234567' ).error, 'phone_number_too_short' );
 	} );
 	test( 'should fail a number containing letters', () => {
-		assert.equal( 'phone_number_contains_letters', phoneValidation( '+123456789a' ).error );
+		assert.strictEqual( phoneValidation( '+123456789a' ).error, 'phone_number_contains_letters' );
 	} );
 	test( 'should fail a number containing special characters', () => {
-		assert.equal(
-			'phone_number_contains_special_characters',
-			phoneValidation( '+(12345)6789' ).error
+		assert.strictEqual(
+			phoneValidation( '+(12345)6789' ).error,
+			'phone_number_contains_special_characters'
 		);
 	} );
 	test( 'should fail an invalid number', () => {
-		assert.equal( 'phone_number_invalid', phoneValidation( '+111111111' ).error );
+		assert.strictEqual( phoneValidation( '+111111111' ).error, 'phone_number_invalid' );
+	} );
+	test( 'should fail an invalid 9-digit argentine no-leading-9 number', () => {
+		assert.strictEqual( phoneValidation( '+54299123456' ).error, 'phone_number_invalid' );
+	} );
+	test( 'should pass a valid 10 digit argentine no-leading-9 number', () => {
+		assert.strictEqual( phoneValidation( '+543511234567' ).info, 'phone_number_valid' );
+	} );
+	test( 'should pass a valid 10 digit argentine plus leading 9 number', () => {
+		assert.strictEqual( phoneValidation( '+5492231234567' ).info, 'phone_number_valid' );
 	} );
 	test( 'should pass a valid number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+447941952721' ).info );
-	} );
-	test( 'should pass a valid 8-digit argentine no-leading-9 number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+5461712345' ).info );
-	} );
-	test( 'should pass a valid 10-digit argentine no-leading-9 number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+54299123456' ).info );
+		assert.strictEqual( phoneValidation( '+447941952721' ).info, 'phone_number_valid' );
 	} );
 	test( 'should pass a valid 8-digit croatian number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+38598123456' ).info );
+		assert.strictEqual( phoneValidation( '+38598123456' ).info, 'phone_number_valid' );
 	} );
 	test( 'should pass a valid 8-digit danish number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+4528123456' ).info );
+		assert.strictEqual( phoneValidation( '+4528123456' ).info, 'phone_number_valid' );
 	} );
 	test( 'should pass a valid 7-digit jamaican number', () => {
-		assert.equal( 'phone_number_valid', phoneValidation( '+18761234567' ).info );
+		assert.strictEqual( phoneValidation( '+18761234567' ).info, 'phone_number_valid' );
+	} );
+	test( 'should pass a valid new format vietnamese number with leading zero', () => {
+		assert.strictEqual( phoneValidation( '+840361234567' ).info, 'phone_number_valid' );
+	} );
+	test( 'should pass a valid new format vietnamese number without leading zero', () => {
+		assert.strictEqual( phoneValidation( '+84361234567' ).info, 'phone_number_valid' );
 	} );
 } );
